92436627586ee95091d8a175fba60a89f32b6168
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2023-02-08 14:56:40 +0300

    Добавлен класс тестирования StandardTest

---
+++ src/test/java/dev/kalenchukov/transliteration/resources/StandardTest.java
@@ -0,0 +1,21 @@
+package dev.kalenchukov.transliteration.resources;
+
+import dev.kalenchukov.transliteration.schemas.Gost7792000ASchema;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Класс проверки констант и методов перечисления {@link Standard}.
+ */
+class StandardTest
+{
+	/**
+	 * Проверка метода {@link Standard#getSchema()}.
+	 */
+	@Test
+	public void getSchema()
+	{
+		assertEquals(new Gost7792000ASchema(), Standard.GOST_7_79_2000A.getSchema());
+	}
+}

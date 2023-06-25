dd548691672328998539a0441f7b01b471d83cb3
Алексей Каленчуков <aleksey.kalenchukov@yandex.ru>
2023-06-25 21:23:04 +0300

    Добавлена аннотация @Unmodifiable в классе RussianGost1687671AScheme

--- src/main/java/dev/kalenchukov/transliteration/schemes/RussianGost1687671AScheme.java
+++ src/main/java/dev/kalenchukov/transliteration/schemes/RussianGost1687671AScheme.java
@@ -25,6 +25,7 @@
 package dev.kalenchukov.transliteration.schemes;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.Map;
 
@@ -45,6 +46,7 @@ public final class RussianGost1687671AScheme extends AbstractScheme
 	 *
 	 * @return {@inheritDoc}
 	 */
+	@Unmodifiable
 	@NotNull
 	@Override
 	public Map<@NotNull String, @NotNull String> getRules()
